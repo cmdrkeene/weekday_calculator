d562d8dafb6b4d8d0df7794fcf0b408cc1819cfa
Brandon Keene <bkeene@gmail.com>
2009-12-12 00:32:22 -0500

    Made algorithm more compact

--- lib/weekday_calculator.rb
+++ lib/weekday_calculator.rb
@@ -5,12 +5,8 @@ module WeekdayCalculator
   # The nth weekday from date
   def self.nth_weekday(nth, date)
     date = date.to_date
-    if date.wday == 6
-      adjustment = 1
-    else
-      adjustment = (2 * ((date.wday + nth - 1).div(5)))
-    end
-    date + nth + adjustment
+    multiplier = date.wday == 6 ? 1 : 2
+    date + nth + (multiplier * (date.wday + nth - 1).div(5))
   end
 
   # Gives you every nth weekday from start to finish
